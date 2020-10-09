18fda7f7d12436f0af5dc599b2f8109d654ad43d
Jinseong Jeon <jsjeon@google.com>
2020-10-09 00:12:44 -0700

    FIR checker: make property init analyzer path-sensitive
    In particular, exception throwing path after finally block is
    distinguished via path label.
    
    #KT-42350 Fixed

--- compiler/tests-spec/testData/diagnostics/notLinked/contracts/analysis/controlFlow/initialization/neg/3.fir.kt
+++ compiler/tests-spec/testData/diagnostics/notLinked/contracts/analysis/controlFlow/initialization/neg/3.fir.kt
@@ -92,10 +92,10 @@ fun case_6() {
     } catch (e: Exception) {
         throw Exception()
     } finally {
-        println(<!UNINITIALIZED_VARIABLE!>value_2<!>.inc())
+        println(value_2.inc())
     }
 
-    <!UNINITIALIZED_VARIABLE!>value_2<!>++
+    value_2++
 }
 
 // TESTCASE NUMBER: 7
